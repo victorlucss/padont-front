fbe164ae460a65831c191fa354f6771f581857a0
Victor Lucas <victorlcmonteiro@gmail.com>
2021-03-22 11:04:42 -0300

    feat: Created markdown view

---
+++ pages/[padName]/view.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useInterval } from "react-use";
+
+import markdownToHtml from 'utils/markdownToHtml'
+import { Content } from 'layouts'
+
+import { padService } from "api";
+
+export default function Pad() {
+  const router = useRouter();
+
+  const [contentHtml, setContentHtml] = useState('');
+
+  const { query } = router;
+
+  const requestPad = async () => {
+    const data = await padService.get(query.padName);
+    const mdToHTML = await markdownToHtml(data.text)
+
+    setContentHtml(mdToHTML)
+  };
+
+  useEffect(() => {
+    requestPad();
+  }, [query.padName]);
+
+  useInterval(() => {
+    requestPad();
+  }, 5000);
+
+  return (
+    <Content dangerouslySetInnerHTML={{ __html: contentHtml }} />
+  );
+}
