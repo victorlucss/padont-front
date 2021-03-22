54b07abf37da33563c7619e079224860a8cb6ceb
Victor Lucas <victorlcmonteiro@gmail.com>
2021-03-22 09:03:05 -0300

    feat: Redirect user to pad when press enter on initial page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 import { Center, Container, Logo } from 'layouts'
 
 import { Input } from 'components'
 
 export default function Home() {
+  const router = useRouter();
+
+
   return (
     <Center>
       <Container max="60%" direction="column" justify="center" height="100vh">
         
         <Logo>{`{ Padont }`}</Logo>
 
-        <Input placeholder="What do you want to create today?" />
+        <Input onPressEnter={(pad) => router.replace(pad)} placeholder="Where do we go? Press enter to continue..." />
       </Container> 
     </Center>
   )
